a9592a48407ac44913d366d5bbc54317285aac4e
Maksym Bordun <max@shorai.sh>
2024-06-14 14:08:57 +0200

    Initial action

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+version="${1}"
+
+if [[ "${version}" != v* ]]; then
+    version="v${version}"
+fi
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${version}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL "https://pkgs.k8s.io/core:/stable:/${version}/deb/Release.key" | sudo gpg --no-tty --batch --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo apt update
+sudo apt -y --allow-downgrades install "kubectl=${version#v}.*"
+
+kubectl version --client=true
